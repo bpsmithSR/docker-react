c6626386139e75e1b3989df47a6df45e5e0da2a1
Blake Smith <bpsmith@solutionreach.com>
2019-07-08 15:40:31 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What time is it? Mess around with <code>src/App.js</code> and save to reload. Then go outside.
         </p>
         <a
           className="App-link"
